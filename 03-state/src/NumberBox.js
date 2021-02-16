6ed325632f07cbc4619afc576b8244a020fbcdea
nhidayahj <hidayah.joha@gmail.com>
2021-02-16 06:53:02 +0000

    react state variable

--- 03-state/src/NumberBox.js
+++ 03-state/src/NumberBox.js
@@ -4,5 +4,36 @@ import React from 'react'
 // a CLASS-based component 
 
 class NumberBox extends React.Component {
-    
+    state = {
+        'count': 10
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div style={{
+                    "border": "2x solid black",
+                    "padding": "10px",
+                    "width": '25px'
+                }}>
+                    {this.state.count}
+                </div>
+                <button onClick={this.increment}>Increment</button>
+            </React.Fragment>
+
+        )
+
+    }
+
+    increment = () => {
+        // we have to use setState to chnage the value of the state variable  
+        //setState is ASYNC 
+        this.setState({
+            'count': this.state.count + 1 
+        })
+    }
 }
+
+
+
+
+export default NumberBox
